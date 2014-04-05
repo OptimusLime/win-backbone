051e9fa335326d33fbb898f29c1ff1e04d339f2c
Paul Szerlip <paul.szerlip@knights.ucf.edu>
2014-04-05 14:09:10 -0400

    0.0.4 -- Now with q callback built in, and only warning for modules that don't have event requirements or event callbacks.
    Easier to build modules now, and easier to use flow control in other modules.

--- test/bb-test.js
+++ test/bb-test.js
@@ -1,65 +1,267 @@
 var winBB = require('../');
-// var colors = require("colors");
+var should = require("should");
 //lets create some sample modules
 var sample = require("./sampleEvo");
-
-// var homePath = path.resolve(__dirname, "..");
-var backbone = new winBB(__dirname + "/");
-var log = backbone.getLogger({winFunction:"mocha"});
-backbone.logLevel = log.testing;
 var path =require('path');
+var colors =require('colors');
 
-var sampleModule = new sample(backbone);
+var	backbone = new winBB(__dirname + "/");
+var saveMod = require('./sampleSave')(backbone);
 
-var sampleJSON = 
-{
-	"evo" : sampleModule,
-	"save" : "./sampleSave.js" 
-};
+var traverse = require('traverse');
 
-//now load in our module file
-backbone.loadModules(sampleJSON);
+backLog = backbone.getLogger({winFunction:"mocha"});
+backbone.logLevel = backbone.testing;
+backLog.logLevel = backLog.testing;
 
-var backEmitter = backbone.getEmitter(sampleModule)
+var backLog, evoEmitter,backEmitter, rModules, sampleJSON;
 
-var callerEvents = backbone.registeredEvents();
-var requiredEvents = backbone.moduleRequirements();
+var wFunToModule;
 
 
-log('All registered functions: ', callerEvents);
-// log('Required Functions/Events: ', requiredEvents);
-for(var wFun in requiredEvents)
-{
-	log('Events required by: ', wFun);
-	log(requiredEvents[wFun]);
+var mochaModule = {
+	winFunction : "mocha",
+	requiredEvents : function(){
+		return [
+		"save:batchSave",
+		"save:manyArgumentCallback",
+		"save:errorCallback"
+		];
+	}
+	// ,eventCallbacks : function(){return {}}
 }
 
+// var homePath = path.resolve(__dirname, "..");
 
 describe('Testing win-backbone',function(){
+
 	before(function(done)
 	{
+		var evoModule = new sample(backbone);
+
+		//modules functions yo
+		rModules = [evoModule.winFunction, saveMod.winFunction, mochaModule.winFunction];
+		wFunToModule = {};
+
+		//not necessarily the real modules
+		wFunToModule[evoModule.winFunction] = evoModule;
+		wFunToModule[saveMod.winFunction] = saveMod;
+		wFunToModule[mochaModule.winFunction] = mochaModule;
+
+		sampleJSON = 
+		{
+			"evo" : evoModule,
+			"save" : "./sampleSave.js",
+			"mocha" : mochaModule
+		};
+
+		//now load in our module file
+		backbone.loadModules(sampleJSON);
+
+		evoEmitter = backbone.getEmitter(evoModule);
+		backEmitter = backbone.getEmitter(mochaModule);
+
 		backbone.initializeModules(function()
     	{
-    		log("Finished Module Init");
+    		backLog("Finished Module Init");
+
  			done();
     	});
-	})
-	it('Should emit without issue',function(done){
 
-		backEmitter.emit('save:batchSave', ["stuffywuffy"], function()
+	})
+
+	it('should have proper modules and events',function(done)
+	{
+		//backwards compat, but more consistent with getters
+		var callerEvents = backbone.getRegisteredEvents();
+		var requiredEvents = backbone.getModuleRequirements();
+
+		backLog('All req functions: '.green, requiredEvents);
+		backLog('All callback functions: '.green, callerEvents);
+		// backLog("Bckbone:", backbone.getModuleCount);
+		// backLog("bb mod:".red, backbone.getModuleCount());
+
+		//check modules -- shoud have enough
+		backbone.getModuleCount().should.equal(rModules.length);
+
+		var allModules = backbone.getModules();
+
+		for(var key in sampleJSON)
 		{
-			log('Batch save test returned: ', arguments);
+			//these are the module names -- check they have been loaded
+			//make sure it exists in the modules we retrieved
+			should.exist(allModules[key]);
+		}
+
+		//now make sure all the required events are correct
+		for(var wFun in wFunToModule)
+		{
+			//what do we require?
+			var mod = wFunToModule[wFun];
+
+			if(!mod.requiredEvents)
+				continue;
+
+			var actualRequired = mod.requiredEvents();
+
+			//shouldn't exist if we don't have any required events
+			if(!actualRequired.length)
+			{
+				should.not.exist(requiredEvents[wFun]);
+				continue;
+			}
+
+			//grab the inner version 
+			var bModRequired = requiredEvents[wFun];
+
+			should.exist(bModRequired);
+
+
+			var all = {};
+			for(var i=0; i < actualRequired.length; i++)
+			{
+				var split = actualRequired[i].split(':');
+				var n = split[0];
+
+				if(!all[n])
+					all[n] = {};
+
+				all[n][split[1]] = actualRequired[i];
+			}
+
+			backLog("Bb required: ".yellow, bModRequired);
+			backLog('Test req functions: '.magenta, actualRequired);
+			backLog("Checking req against: ".magenta, all);
+
+			//catch all the required
+			for(var mReqFunc in all)
+			{
+				var actual = all[mReqFunc];
+				var gen = bModRequired[mReqFunc];
+
+				backLog("inner test: ".magenta, actual)
+				backLog("inner backbone: ".yellow, gen)
+
+				//exists, and has same nubmer of keys
+				should.exist(gen);
+				
+				Object.keys(actual).length.should.equal(Object.keys(gen).length);
+
+				for(var k in actual)
+					actual[k].should.equal(gen[k]);
+			}
+		}
+
+		//lets squish callbacks to do this quicker
+		var squishCallbackNames = [];
+		traverse(callerEvents).forEach(function(node)
+		{
+			if(this.isLeaf)
+				squishCallbackNames.push(this.node);
+		});
+
+		var squishActual = [];
+		for(var modFunc in wFunToModule)
+		{
+			//now lets pull all callbacks
+			var mod = wFunToModule[modFunc];
+			if(!mod.eventCallbacks)
+				continue;
+
+			//get callbacks
+			var cbs = mod.eventCallbacks();
+
+			//if you don't have cbs dont add please
+			if(Object.keys(cbs).length)
+				squishActual = squishActual.concat(Object.keys(cbs));
+		}
+		var noDups = {};
+		traverse(squishActual).forEach(function(){
+			if(this.isLeaf)
+			{
+				//no more you
+				if(noDups[this.node])
+					this.remove();
+
+				noDups[this.node] = true;
+			}
+		});
+		backLog("Test calbacks : ".magenta, squishActual);
+		backLog("Registered callbacks :".yellow, squishCallbackNames);
+
+		squishActual.length.should.equal(squishCallbackNames.length);
+		squishActual.sort();
+		squishCallbackNames.sort();
+
+		squishActual.join(',').should.equal(squishCallbackNames.join(','));
+
+		//all done with registered stuff
+		done();
+
+	});
+
+	it('Should emit from eveolution module without issue',function(done){
+
+		//call from "inside" another module 
+		evoEmitter.emit('save:batchSave', ["stuffywuffy"], function()
+		{
+			backLog('Batch save test returned: ', arguments);
 			done();
 		});
     });
 
+	it('Should used q callback to return function call', function(done){
+
+
+		//shoudl call qfunction
+		var incomingArgs = ["stuffywuffy"];
+
+		var eMessage = "error";
+
+		backLog("Making many arg callback: ".magenta, incomingArgs);
+
+		backEmitter.qCall('save:manyArgumentCallback', incomingArgs[0])
+			.then(function(repeat)
+			{	
+				backLog("Many call return: ".yellow, repeat);
+				//this is a single return call 
+				incomingArgs[0].should.equal(repeat);
+
+				incomingArgs.push("new arg");
+
+				return backEmitter.qCall('save:manyArgumentCallback', incomingArgs[0], incomingArgs[1])
+			})
+			.then(function(multi)
+			{
+				incomingArgs[0].should.equal(multi[0]);
+				incomingArgs[1].should.equal(multi[1]);
+				return backEmitter.qCall('save:errorCallback', incomingArgs[0]);
+			})
+			.fail(function(err)
+			{
+				backLog("Err: ", err);
+				//should catch this error -- if it isn't the error we purposefully threw, it will also cause an error here too
+				err.message.should.equal(eMessage);
+
+				//survied the test
+				backLog("Finised Q flow function tests");
+			})
+			.done(function()
+			{
+				backLog("it's finally over");
+				done();
+			})
+
+	})
+
+
     it('Should emit error due to lack of event permission',function(done){
 
-		log('Test invalid callback');
+		backLog('Test invalid callback');
 
 		try
 		{
-			backEmitter('evolution:createIndividual', 0, function()
+			evoEmitter('evolution:createIndividual', 0, function()
 			{
 				log('invalid callback');
 			});
--- test/sampleSave.js
+++ test/sampleSave.js
@@ -26,7 +26,32 @@ function sampleSave(backbone)
 		done("artifacts fetched now");
 	}
 
-	
+	function mirrorCallback()
+	{
+		self.log("Mirror o args: ", arguments);
+		//pull off first argument
+		var shouldError = [].shift.call(arguments);
+
+		//pop the last arguemnt -- the callback
+		var finished = arguments[arguments.length-1];
+
+		//then apply liberally popping off
+		[].pop.call(arguments);
+
+		if(shouldError)
+		{
+			[].splice.call(arguments, 0, 0, new Error("error"));
+		}
+		else
+		{
+			//push in no error!
+			[].splice.call(arguments, 0, 0, undefined);
+		}
+
+		self.log("mirror callback made: ".cyan, arguments);
+
+		finished.apply(self, arguments);
+	}
 	//we are evolution
 	//these are the various callbacks we accept as events
 	self.eventCallbacks = function()
@@ -34,6 +59,16 @@ function sampleSave(backbone)
 		var callbacks = {};
 
 		//add callbacks to the object-- these are the functions called when the full event is emitted
+		callbacks[fullEventName("manyArgumentCallback")] = function() { 
+			//this time don't cause an eerror -- just mirror
+			[].splice.call(arguments, 0, 0, false);
+			mirrorCallback.apply(self, arguments)
+		};
+		callbacks[fullEventName("errorCallback")] =  function() { 
+			//this time cause an error
+			[].splice.call(arguments, 0, 0, true);
+			mirrorCallback.apply(self, arguments)
+		};
 		callbacks[fullEventName("batchSave")] = batchSave;
 		callbacks[fullEventName("getArtifacts")] = getArtifacts;
 
